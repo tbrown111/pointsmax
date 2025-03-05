ee381fc13f5151ffb89ba60ee026682cfd55fba9
Saim Bhimji <bhimjisaim@gmail.com>
2025-03-05 13:38:22 -0500

    Backend init

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express();
+
+//for local hosting --> remove when hosting on firebase
+const PORT = 3000;
+
+app.listen(PORT, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running, and App is listening on port "+ PORT)
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
+
+app.get('/test', (req, res) => {
+    res.json({ value: 'Test' });
+});
